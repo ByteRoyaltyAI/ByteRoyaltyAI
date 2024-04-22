41ae6ef194ae65e009fa2daf4982626d0390329f
ShaikMohammadUsman <63662151+ShaikMohammadUsman@users.noreply.github.com>
2024-04-22 12:17:51 +0530

    Update Footer.tsx
    Updated Phone number,Linkedin,Twitter,Company name,Email etc in website footer code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,16 +8,16 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="bg-darkblue p-10 max-lg:flex-col max-lg:pb-48 relative z-[1]">
-      <div className="text-white text-xl mb-10 text-center" style={{fontSize:"15px"}}>Hyderabad, India | Phone: +91 - 6303438141 | Email: Info@Futureisai.Tech</div>
-      <div className="opacity-60 text-white text-base mb-6 text-center" style={{fontSize:"15px"}}>Copyright © 2024 FutureisAI. All Rights Reserved.</div>
+      <div className="text-white text-xl mb-10 text-center" style={{fontSize:"15px"}}> India | Phone: +91 - 8179871764 | Email: support@imaigen.ai </div>
+      <div className="opacity-60 text-white text-base mb-6 text-center" style={{fontSize:"15px"}}>Copyright © 2024 IMAIGEN. All Rights Reserved.</div>
       <div className="flex justify-center space-x-6">
-        <a href="https://www.linkedin.com/company/futureisai/">
+        <a href="https://www.linkedin.com/company/102466043/admin/notifications/all/">
           <FaLinkedin style={linkedinStyle} />
         </a>
-        <a href="https://x.com/futureisai_tech?t=GEYakisBsitt9oq5pTJUdA&s=09">
+        <a href="https://twitter.com/imaigen4">
           <FaTwitter style={twitterStyle} />
         </a>
-        <a href="https://instagram.com/futureisai.tech?igshid=OGQ5ZDc2ODk2ZA">
+        <a href="https://www.instagram.com/imaigen4/">
           <FaInstagram style={instagramStyle} />
         </a>
       </div>
