c553021001d0ae3e9851e5399c60762671078c8d
ShaikMohammadUsman <63662151+ShaikMohammadUsman@users.noreply.github.com>
2024-04-30 16:44:01 +0530

    Update Contact.tsx
    Updated the recent url in this file

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -44,7 +44,7 @@ const Contact: React.FC = () => {
     try {
 
       console.log(JSON.stringify(data));
-      const response = await axios.post("https://imaigen-websitee-backend.vercel.app/send-email", data);
+      const response = await axios.post("https://imaigen-websitee-backend.onrender.com/send-email", data);
       console.log(response);
       if (language === "DE") {
         toast.success(toastMessages.successEmailSent.de);
