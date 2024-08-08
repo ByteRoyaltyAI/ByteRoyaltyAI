4543bfc7e1a8bc87bc57ad99059fbe4e97707d5f
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-08 12:19:10 +0500

    Some comments removed, related theme.

---
+++ src/pages/CareerAdvice.tsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+const CareerAdvice = () => {
+  return (
+    <div className='flex justify-center  min-h-screen mt-32 bg-[#0C1220] '>
+      <div className="flex flex-col gap-16 w-[80%] text-[#FFFFFF]">
+     
+     <div className="flex flex-col gap-5 mt-40">
+     <h1 className="font-bold text-[4rem]">
+     Custom AI Agents
+     </h1>
+     <h3 className="text-[2rem] text-[#E8E8E8]">Create custom AI agents to automate tasks and boost productivity.</h3>
+     <h3 className="text-[1.6rem] text-[#E8E8E8] mt-5">Updated over a week ago</h3>
+     </div>
+
+     <div className="flex flex-col gap-5">
+     <h2 className="font-semibold" >What Are Custom Agents?</h2>
+     <h3 className="text-[2rem] text-[#E8E8E8]">AI Agents are specialized assistants designed to automate activities like research, data analysis, and content creation. You can create custom AI Agents and use them in your projects to save time and get more done faster and smarter.</h3>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default CareerAdvice
