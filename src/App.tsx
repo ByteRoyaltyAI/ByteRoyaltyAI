531adc2c1cb90637de0196897520a69db9fed3b2
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-10 11:00:00 +0500

    New Book A DemoPage Added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LanguageContextProvider from "./context/language-context";
 import SubProductPage from "./pages/SubProductPage"
 import Newnavbar from "./components/Newnavbar";
 import ProductsPage from "./pages/ProductsPage";
+import BookADemoPage from "./pages/BookADemoPage";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/privacy" element={<Home />}></Route>
                 <Route path="/product/:hr-services" element={<ProductsPage />}></Route>
                 <Route path="/product/:productName/:subProduct" element={<SubProductPage />}></Route>
+                <Route path="/product/book-a-demo" element={<BookADemoPage />}></Route>
               </Routes>
             </ActiveSectionContextProvider>
           </LanguageContextProvider>
