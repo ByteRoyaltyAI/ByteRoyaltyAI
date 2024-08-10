2099d09049d6722073288dde71216174721d8a92
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-10 14:50:09 +0500

    New Gradient Hover Added

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -2,6 +2,9 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import { motion } from 'framer-motion';
+import img1 from "../assets/productImages/img1.jpg"
+import img2 from "../assets/productImages/img2.jpg"
+import img3 from "../assets/productImages/img3.jpg"
 
 const ProductsPage = () => {
     // const [activeBtn,setActiveButton]=useState<string>("RJM")
@@ -24,8 +27,9 @@ const ProductsPage = () => {
             <div className='flex flex-col md:flex-row justify-center items-center w-[90%] gap-14 border-[#131C35] border-b-2 pb-16'>
             {data.map((card,index)=>(
                 <Link to={"/product/hrServices/"+card.link} key={index} className="w-full sm:w-3/4 md:w-[30%] ">
-                <div className='flex flex-col  p-10 gap-5 rounded-lg  bg-[#1b1b28] hover:bg-[#1d1d2c] min-h-[360px]'>
-                  <img className='object-contain '
+                <div className='flex flex-col  p-10 gap-7 rounded-lg  bg-[#1b1b28]  min-h-[360px] hover:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#4b5364] to-[#0D161C]'>
+                  <img src={card.img}
+                  className='object-contain h-[180px] w-[320px] bg-white'
                   alt='product here'
                   />
                   <p className='font-semibold text-[18px] '>{card.title}</p>
@@ -45,19 +49,19 @@ let data=[
     {
     title:"Resume-Job Matching",
     desc:"Use AI to automatically match resumes with job descriptions based on skills, experience, and qualifications. Improve candidate fit and reduce turnover.",
-    img:"",
+    img:img1,
     link:"resumeMatching"
 },
     {
     title:"Interview Question Generation",
     desc:"Automatically generate relevant interview questions based on the candidate’s resume and the job description. Ensure consistency and fairness in the interview process.",
-    img:"",
+    img:img2,
     link:"interviewGeneration"
 },
     {
     title:"Hr Chatbot",
     desc:"Leverage AI to create an HR chatbot that can answer employee queries, assist with onboarding, and automate routine HR tasks.",
-    img:"",
+    img:img3,
     link:"hrChatbot"
 },
 ]
