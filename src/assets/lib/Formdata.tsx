00a8680dd485b261606fcb8d84964e0da34e84e3
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-14 16:20:12 +0500

    Form Data updated

--- src/assets/lib/Formdata.tsx
+++ src/assets/lib/Formdata.tsx
@@ -2,7 +2,7 @@
 interface InputField {
     id: string;
     label: string;
-    type: 'number' | 'text'; 
+    type: 'number' | 'text' | "boolean"; 
     placeholder: string;
 }
 
@@ -105,4 +105,113 @@ export const formInputData: Record<string, FormSection> = {
             },
         ],
     },
+    loanEligibility: {
+        title: "Loan Eligibilty Assesment",
+        endPoint: "loan_eligibility_assesment",
+        inputFields: [
+            {
+                id: "annual_income",
+                label: "Annual Income",
+                type: "number",
+                placeholder: "Enter your annual income",
+            },
+            {
+                id: "credit_score",
+                label: "Credit Score",
+                type: "number",
+                placeholder: "Enter your credit score",
+            },
+            {
+                id: "existing_debt",
+                label: "Existing Debt",
+                type: "number",
+                placeholder: "Enter your existing debt",
+            },
+            {
+                id: "employment_status",
+                label: "Employment Status",
+                type: "text",
+                placeholder: "Enter your employment status",
+            },
+            {
+                id: "other_relevant_info",
+                label: "Other Relevant Information",
+                type: "text",
+                placeholder: "Enter other relevant info",
+            },
+            {
+                id: "required_income",
+                label: "Required Income",
+                type: "number",
+                placeholder: "ex. 60,000",
+            },
+            {
+                id: "minimum_credit_score",
+                label: "Minimum Credit Score",
+                type: "number",
+                placeholder: "ex. 30,000",
+            },
+            {
+                id: "max_debt_income_ratio",
+                label: "Max Debt Income Ratio",
+                type: "number",
+                placeholder: "ex. 30,000",
+            },
+            {
+                id: "reqd_employment_status",
+                label: "Required Employment Status",
+                type: "text",
+                placeholder: "ex. employed",
+            },
+        ],
+    },
+    careerAdvice: {
+        title: "Career Advice ",
+        endPoint: "career_advice",
+        inputFields: [
+            {
+                id: "Current_profession",
+                label: "Current Profession",
+                type: "text",
+                placeholder: "Enter your current profession",
+            },
+            {
+                id: "Yrs_Experience",
+                label: "Years of Experience",
+                type: "number",
+                placeholder: "Enter your years of experience",
+            },
+            {
+                id: "Location",
+                label: "Location",
+                type: "text",
+                placeholder: "Enter your location",
+            },
+            {
+                id: "Geographical",
+                label: "Geographical Preferences",
+                type: "text",
+                placeholder: "Enter your geographical preferences",
+            },
+            {
+                id: "Commitments",
+                label: "Personal Commitments",
+                type: "text",
+                placeholder: "Enter your personal commitments",
+            },
+            {
+                id: "Experienced_or_Fresher",
+                label: "Experienced or Fresher",
+                type: "text",
+                placeholder: "Enter true for experienced, false for fresher",
+            },
+            {
+                id: "Interests",
+                label: "Interests",
+                type: "text",
+                placeholder: "Enter your interests",
+            },
+        ],
+    }
+    
 };
