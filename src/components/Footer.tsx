076f91d85993cb508983d9a7647ee501bf6503ca
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-16 10:33:10 +0500

    chatbot commented out

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
-import Chatbot from "./Chatbot";
+// import Chatbot from "./Chatbot";
 
 const Footer: React.FC = () => {
   const linkedinStyle = { color: "#0077B5" };
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
           <FaInstagram style={instagramStyle} />
         </a>
       </div>
-      <Chatbot/>
+      {/* <Chatbot/> */}
     </footer>
   );
 };
