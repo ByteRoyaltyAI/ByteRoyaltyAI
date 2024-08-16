56e2efc395caeb008c80ce3f83584fad8d80d7f7
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-16 10:14:58 +0500

    chatbot added

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -7,29 +7,39 @@ const Chatbot = () => {
   return (
     <div className="relative">
       {isOpen ?
-      <IoLogoOctocat className="fixed absoute bottom-6 right-7  h-20 w-20 transform hover:scale-105 "
+      <IoLogoOctocat className="fixed  bottom-6 right-7  h-20 w-20 transform hover:scale-105 "
       onClick={()=>setIsOpen(!isOpen)}/>
       :
-        <div className="fixed absoute bottom-16 right-16 z-10  h-[55rem] w-[38rem] bg-[#1A2238] rounded-lg shadow-gray-600 shadow-lg animate-slideDown">
-         <svg
-            className="absolute h-12 w-12 right-5 top-5"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-            onClick={()=>setIsOpen(!isOpen)}
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-            />
-        </svg>
-
+        <div className="fixed  bottom-16 right-6  sm:right-16 z-[60]  h-[70%] w-[90%] sm:h-[80%] sm:w-[40rem] bg-[#1A2238] rounded-lg shadow-gray-600 shadow-lg animate-slideDown">
+         <CloseIcon setIsOpen={setIsOpen} isOpen={isOpen}/>
+        
       </div>}
     </div>
+  )
+}
+type CloseIconProps = {
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOpen: boolean;
+};
+
+const CloseIcon: React.FC<CloseIconProps>=({setIsOpen,isOpen})=>{
+  return (
+      <svg
+        className="absolute h-12 w-12 right-5 top-5"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+        onClick={()=>setIsOpen(!isOpen)}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+        />
+    </svg>
   )
 }
 
