5cebcaad32270a7af6f8735cfb0917b5ecbf0fbb
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-19 11:01:18 +0500

    New Messages component added

---
+++ src/components/Messages.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { IoLogoOctocat } from "react-icons/io5";
+
+let messageText =
+  "ml-5 text-black bg-[#1A2238] font-sans text-left p-6 text-[16px] rounded-xl max-w-[80%] ";
+let messageTextOwn =
+  "ml-5 text-black bg-[#1A2238] font-sans text-left p-6 text-[16px] rounded-xl max-w-[80%]";
+
+type Props = {
+  own: boolean;
+  message: string;
+};
+
+const Messages: React.FC<Props> = ({ own, message }) => {
+  return (
+    <div className="">
+      <div className={`flex p-2 ${own ? "justify-end" : ""}`}>
+      {!own &&
+      <div className="flex justify-center items-center relative h-16 w-16 rounded-full bg-white">
+      <IoLogoOctocat className="h-12 w-12  text-[#1A2238]  "/>
+      </div>}
+
+        {message && (
+          <p className={own ? messageTextOwn : messageText}>{message}</p>
+        )}
+      </div>
+      <div className={`flex  ${own ? "justify-end" : ""}`}>
+        {/* <p className="text-sm ml-3 text-white ">{message.time}</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
