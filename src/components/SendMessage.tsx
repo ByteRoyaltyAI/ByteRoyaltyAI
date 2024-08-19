30e3d4c07a1b78d3b175676c791be3092e79847c
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-19 11:01:31 +0500

    New send message component added

---
+++ src/components/SendMessage.tsx
@@ -0,0 +1,45 @@
+import { KeyboardEvent, FormEvent, useState } from "react";
+import { IoSend } from "react-icons/io5";
+
+interface SendMessageProps {
+  setMessages: React.Dispatch<React.SetStateAction<{ own: boolean; text: string }[]>>;
+}
+
+const SendMessage: React.FC<SendMessageProps> = ({ setMessages }) => {
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSendMessage = (e?: FormEvent) => {
+    e?.preventDefault();
+    if (newMessage.trim()) {
+      // Add the new message to the chat
+      setMessages((prev) => [...prev, { own: true, text: newMessage }]);
+      setNewMessage("");
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      handleSendMessage();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSendMessage} className="flex gap-2 p-3 bg-[#1A2238]">
+      <input
+        onChange={(e) => setNewMessage(e.target.value)}
+        onKeyDown={handleKeyDown}
+        type="text"
+        value={newMessage}
+        placeholder="Type a message"
+        className="p-4 rounded-md sm:w-full w-[50%] h-20 text-[15px] bg-[#FFFFFF] placeholder-[#667781] border border-[#dfe3e8]"
+        style={{ color: "black" }}
+      />
+      <IoSend
+        onClick={handleSendMessage}
+        className="p-2 h-16 w-16 hover:cursor-pointer text-white rounded-lg mt-1 hover:text-gray-200"
+      />
+    </form>
+  );
+};
+
+export default SendMessage;
