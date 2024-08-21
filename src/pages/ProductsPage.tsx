8bb68596bb45dac63839f59e7536077699a8eee1
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-21 13:25:50 +0500

    cards display changed from flex to grid

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -45,9 +45,9 @@ const ProductsPage: React.FC = () => {
       </div>
 
       <div className='flex justify-center mb-32'>
-        <div className='flex flex-col md:flex-row justify-center items-center w-[90%] gap-14 pb-16'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center  gap-10 lg:gap-20 xl:gap-32 w-[90%] pb-16'>
           {productData.cards?.map((card, index) => (
-            <Link to={`/product/hrServices/${card.link}`} key={index} className="w-full sm:w-3/4 md:w-[30%]">
+            <Link to={`/product/hrServices/${card.link}`} key={index} className="">
               <div className='flex relative flex-col p-10 gap-7 rounded-lg bg-[#1b1b28] min-h-[360px] hover:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#4b5364] to-[#0D161C]'>
                 <img
                   src={card.img}
