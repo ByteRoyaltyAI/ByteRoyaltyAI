d6d7f93bf3d211ad0ebefee2b6535bea8dc1e6da
Rafay0237 <143127689+Rafay0237@users.noreply.github.com>
2024-08-21 13:11:36 +0500

    Dynammic headings updated

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import { motion } from 'framer-motion';
-import { productCardsData } from "../assets/lib/ProductsData";
+import { ProductPageData } from "../assets/lib/ProductsData";
 import { useParams,useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import NeoAI from '../components/NeoAI';
@@ -14,9 +14,9 @@ const ProductsPage: React.FC = () => {
   const params=useParams<{ productName: string }>()
   const navigate=useNavigate()
   
-  const cardsData= productCardsData[params.productName as string] ;
+  const productData=ProductPageData[params.productName as string] 
 
-  if(cardsData===undefined){
+  if(productData===undefined){
     useEffect(()=>{
       navigate("/")
     },[])
@@ -28,9 +28,9 @@ const ProductsPage: React.FC = () => {
       <div className='flex justify-center'>
         <NeoAI/>
         <div className='flex flex-col items-center w-[90%] sm:w-[70%] gap-10'>
-          <AnimatedHeading />
+          <AnimatedHeading title={productData.title}/>
           <h1 className="font-bold text-center mb-40" style={{fontSize: "clamp(1.8rem, 5vw, 2.4rem)"}}>
-          <span className="remainText ">Streamline hiring, enhance candidate selection, and empower your HR team with AI-driven tools. </span>
+          <span className="remainText ">{productData.desc}</span>
         </h1>
 
             <Button
@@ -44,7 +44,7 @@ const ProductsPage: React.FC = () => {
 
       <div className='flex justify-center mb-32'>
         <div className='flex flex-col md:flex-row justify-center items-center w-[90%] gap-14 pb-16 '>
-          {cardsData.map((card, index) => (
+          {productData.cards?.map((card, index) => (
             <Link to={`/product/hrServices/${card.link}`} key={index} className="w-full sm:w-3/4 md:w-[30%] ">
               <div className='flex  relative flex-col p-10 gap-7 rounded-lg bg-[#1b1b28] min-h-[360px] hover:bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#4b5364] to-[#0D161C]'>
                 <img
@@ -66,13 +66,8 @@ const ProductsPage: React.FC = () => {
   );
 };
 
-const AnimatedHeading: React.FC = () => {
-  const letters = [
-    'T', 'r', 'a', 'n', 's', 'f', 'o', 'r', 'm', ' ',
-    'Y', 'o', 'u', 'r', ' ',
-    'H', 'i', 'r', 'i', 'n', 'g', ' ',
-    'P', 'r', 'o', 'c', 'e', 's', 's'
-  ];
+const AnimatedHeading: React.FC<{title:string}> = ({title}) => {
+  const letters = title.split("")
 
   return (
     <div className=" text-center " 
