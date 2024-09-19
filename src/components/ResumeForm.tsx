a9aededc04ffd9a36eb1d3a1121031743f0ca820
ShaikMohammadUsman <63662151+ShaikMohammadUsman@users.noreply.github.com>
2024-09-19 12:56:01 +0530

    Update ResumeForm.tsx
    Currently updated the env variable to actual ip address of server

--- src/components/ResumeForm.tsx
+++ src/components/ResumeForm.tsx
@@ -61,7 +61,8 @@ const ResumeJobMatcher: React.FC = () => {
       formData.append('difficulty', difficulty);
     }
 
-    const API_URL = import.meta.env.VITE_AI_API_URL + (isInterviewGeneration ? 'interview_generation' : 'extract_and_analyze');
+    // const API_URL = import.meta.env.VITE_AI_API_URL + (isInterviewGeneration ? 'interview_generation' : 'extract_and_analyze');
+    const API_URL = "http://localhost:8000/api/extract_and_analyze"
     
     try {
       const response = await axios.post<MatchResult>(
